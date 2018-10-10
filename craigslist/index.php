165be05caacf8921f0510429d8a4ef92ae47f844
CJ Stritzel <cjstritzel@Admins-MacBook-Pro.local>
2018-10-10 11:15:04 -0700

    adding instructions and links to index

--- craigslist/index.php
+++ craigslist/index.php
@@ -1,8 +1,24 @@
-<pre><?php
-  require_once('Classes/craigslist.php');
-  $CL = new Craigslist();
-  print_r($CL->urls());
-?>
-</pre>
+<h1>Craigslist "gigs"</h1>
 
-index.php
+<p>Use browser to get Craigslist "gigs."
+
+<h2>Sample URL's</h2>
+<ul>
+  <li>
+    <a href="gigs.php">
+      gigs.php | Portland | Wordpress
+    </a>
+  </li>
+
+  <li>
+    <a href="gigs.php?city=losangeles&query=react">
+      gigs.php?city=losangeles&query=react | Los Angeles | React
+    </a>
+  </li>
+
+  <li>
+    <a href="gigs.php?city=newyork&query=shopify">
+      gigs.php?city=losangeles&query=react | New York | Shopify
+    </a>
+  </li>
+</ul>
