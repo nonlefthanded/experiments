b423005e55ca2c24e870f4dc55beafe48fdf01fb
CJ Stritzel <cjstritzel@Admins-MacBook-Pro.local>
2018-10-10 11:19:25 -0700

    adding instructions and links to index

--- craigslist/index.php
+++ craigslist/index.php
@@ -3,6 +3,7 @@
 <p>Use browser to get Craigslist "gigs" search results, in JSON format.
 
 <h2>Sample URL's</h2>
+<p>The var "city" needs to be a correct Craigslist city subdomain. The var "query" can be whatever you want...
 <ul>
   <li>
     <a href="gigs.php">
