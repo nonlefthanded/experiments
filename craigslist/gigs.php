d1b35477cab112283091cf406aa37b24e80678e9
CJ Stritzel <cjstritzel@Admins-MacBook-Pro.local>
2019-03-07 14:11:32 -0800

    adding Access Control headers to gigs.php

--- craigslist/gigs.php
+++ craigslist/gigs.php
@@ -3,7 +3,7 @@
   header("Content-type:application/json");
   require_once('Classes/craigslist.php');
   $query = (!isset($_GET['query'])) ? 'wordpress' : $_GET['query'];
-  $city  = (!isset($_GET['city'])) ? 'portland' : $_GET['city']; 
+  $city  = (!isset($_GET['city'])) ? 'portland' : $_GET['city'];
   $clUrl = sprintf('https://%s.craigslist.org/search/cpg?format=rss&is_paid=all&query=%s',$city,$query);
   $cityInfo = new ParseRSS();
   echo $cityInfo->data($clUrl);
